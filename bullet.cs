678b4e86b5004adce3afc39336968de14644f1ba
Bodula-sravani <sravanibodula@gmail.com>
2023-03-23 17:15:18 +0530

    Bullet program file

---
+++ bullet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+	}
+
+	/* public static void Main(string[] args)
+	{
+		int hero = 1;
+		int villain = 1;
+		var hero_bullets = new List<int>();
+		var villain_bullets = new List<int>();
+		Console.WriteLine("Enter number of bullets");
+		int no_of_bullets = Convert.ToInt32(Console.ReadLine());
+		do
+		{
+			if (hero <=no_of_bullets)
+			{
+				hero_bullets.Add(hero);
+				hero += 1;
+			}
+			if (villain <=no_of_bullets)
+			{
+				villain_bullets.Add(villain);
+				villain += 3;
+			}
+		} while (hero <=no_of_bullets || villain <=	no_of_bullets);
+
+		Console.WriteLine();
+		Console.WriteLine("No of shots taken by hero: " + hero_bullets.Count);
+		Console.WriteLine("No of shots taken by villain: " + villain_bullets.Count);
+		Console.WriteLine();
+		Console.WriteLine("Bullet numbers of hero at each shot: ");
+		foreach(int i in hero_bullets)
+		{
+			Console.Write(i+" ");
+		}
+
+		Console.WriteLine();
+        Console.WriteLine("Bullet numbers of villian at each shot: ");
+		foreach(int i in villain_bullets)
+		{
+			Console.Write(i + " ");
+		}
+    }
+	*/
+}
