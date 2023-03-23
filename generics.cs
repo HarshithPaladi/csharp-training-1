6d009fd3af5aea9f72707ad9c2f61d15ba3395ca
Bodula-sravani <sravanibodula@gmail.com>
2023-03-23 15:21:52 +0530

    generics

---
+++ generics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class myGeneric<T>
+{
+	public myGeneric(T message)
+	{
+		Console.WriteLine(message);
+	}
+
+}
+
+public class generics
+{
+	/*
+	public static void Main(string[] args)
+	{
+		myGeneric<int> g1 = new myGeneric<int>(1);
+		myGeneric<string> g2 = new myGeneric<string>("sravani");
+
+	}
+}
+
+*/
+}
