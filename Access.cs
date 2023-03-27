5968d3582bf53e787dc9478813a0d7de7d908891
Bodula-sravani <sravanibodula@gmail.com>
2023-03-27 10:41:36 +0530

    Access modifiers

---
+++ Access.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Access
+{
+	protected internal string name = "access";
+	public Access()
+	{
+	}
+
+	protected internal void display(string s)
+	{
+		Console.WriteLine(s);
+	}
+}
